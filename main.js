4947f5709074afb887b038a46e3831de3538218d
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-09 16:59:03 -0300

    ToDo
    Temos que descobrir como vamos adicionar novos elementos ao DOM, corrigirei este codigo amanha para finalizar a tarefa de mostrar a playlist

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, webContents} = require('electron');
 const request = require('request');
 require('dotenv').config();
 
 (function getPlaylist(){
     const urlPlaylist = "https://api.spotify.com/v1/me/playlists";
-    const token = 'Bearer ' + process.env.TOKEN
-    console.log(token)
+    const token = 'Bearer ' + process.env.TOKEN;
     request({url: urlPlaylist, method: 'GET', headers: {'Authorization': token, 'Content-Type': 'application/json'}},
-        function (error, response, body) {
-            const result = JSON.parse(body)
-            for(i=0; i < result.items.length; i++) {
-                console.log(result.items[i].name)
-            }
-        })
-})()
+    function (error, response, body) {
+        if(error) return;
+        const result = JSON.parse(body);
+        for(i=0; i < result.items.length; i++) {
+            
+        }
+    });
+})();
 
+function createPlaylistName(name){
+    const playlist = document.querySelector('#playlist');
+    const a = document.createElement('a');
+    a.innerHTML = name;
+    a.className = 'bar-library btn';
+    playlist.appendChild(a)
+    
+}
 
 
 //Configurações do Electron
+let mainWindow;
 function createWindow(){
-    const win = new BrowserWindow({
-        width: 800,
-        heigth: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
+    app.on('ready', () => {
+        mainWindow = new BrowserWindow({
+            width: 800,
+            height: 600,
+            frame: true,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
+        return mainWindow;
     })
-    win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow);
+mainWindow = createWindow();
+console.log(mainWindow.webContents)
 
 app.on('window-all-close', () => {
     if(process.platform !== 'darwin'){
@@ -39,6 +53,6 @@ app.on('window-all-close', () => {
 
 app.on('activate', () => {
     if(BrowseWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
 })
