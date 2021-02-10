66f83a5be26157bdc6a8d7012cde6e0b46e83060
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-10 10:21:37 -0300

    Criação dos nomes da playlist
    Foram criados os nomes das playlists porem falta arrumar o css

---
+++ getPlaylist.js
@@ -0,0 +1,22 @@
+const request = require('request');
+require('dotenv').config();
+
+const urlPlaylist = "https://api.spotify.com/v1/me/playlists";
+const token = 'Bearer ' + process.env.TOKEN;
+request({url: urlPlaylist, method: 'GET', headers: {'Authorization': token, 'Content-Type': 'application/json'}},
+function (error, response, body) {
+    if(error) return;
+    const result = JSON.parse(body);
+    for(i=0; i < result.items.length; i++) {
+        createPlaylistName(result.items[i].name)        
+    }
+});
+
+function createPlaylistName(name){
+    const playlist = document.querySelector('#playlist');
+    const a = document.createElement('a');
+    a.innerHTML = name;
+    a.className = 'bar-library btn';
+    playlist.appendChild(a)    
+}
+
--- main.js
+++ main.js
@@ -1,28 +1,4 @@
 const {app, BrowserWindow, webContents} = require('electron');
-const request = require('request');
-require('dotenv').config();
-
-(function getPlaylist(){
-    const urlPlaylist = "https://api.spotify.com/v1/me/playlists";
-    const token = 'Bearer ' + process.env.TOKEN;
-    request({url: urlPlaylist, method: 'GET', headers: {'Authorization': token, 'Content-Type': 'application/json'}},
-    function (error, response, body) {
-        if(error) return;
-        const result = JSON.parse(body);
-        for(i=0; i < result.items.length; i++) {
-            
-        }
-    });
-})();
-
-function createPlaylistName(name){
-    const playlist = document.querySelector('#playlist');
-    const a = document.createElement('a');
-    a.innerHTML = name;
-    a.className = 'bar-library btn';
-    playlist.appendChild(a)
-    
-}
 
 
 //Configurações do Electron
@@ -38,12 +14,10 @@ function createWindow(){
             }
         });
         mainWindow.loadURL('file://' + __dirname + '/index.html');
-        return mainWindow;
     })
 }
 
 mainWindow = createWindow();
-console.log(mainWindow.webContents)
 
 app.on('window-all-close', () => {
     if(process.platform !== 'darwin'){
