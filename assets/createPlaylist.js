6c7e75b956ad0861eee24e50c9f2636212909a88
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-11 10:00:13 -0300

    Mensagem de erro
    html e css da mensagem de erro do CreatePlaylist

--- assets/createPlaylist.js
+++ assets/createPlaylist.js
@@ -5,3 +5,10 @@ function showCreatePlaylist(){
 function hiddenCreatePlaylist() {
     document.querySelector('.popup').style.display = 'none';
 }
+
+function createPlaylist() {
+    const name = document.querySelector('#name').value
+    if(!name) {
+        const errorText = document.querySelector('.errorText')
+    }
+}
