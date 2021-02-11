43f4513f5e38cf6d0dfbc720bd55869a628c4584
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-11 10:46:54 -0300

    Regra 4 tab eslint

--- assets/createPlaylist.js
+++ assets/createPlaylist.js
@@ -1,24 +1,25 @@
+const request = require('request');
+require('dotenv').config();
+
 function showCreatePlaylist() {
-  document.querySelector('.popup').style.display = 'block';
+    document.querySelector('.popup').style.display = 'block';
 }
 
 function hiddenCreatePlaylist() {
-  document.querySelector('.popup').style.display = 'none';
+    document.querySelector('.popup').style.display = 'none';
 }
 
 function createPlaylist() {
-  const name = document.querySelector('#name').value;
-  console.log(name);
-  if (!name) {
-    document.querySelector('.errorText').style.display = 'block';
-  }
+    const name = document.querySelector('#name').value;
+    if (!name) {
+        document.querySelector('.errorText').style.display = 'block';
+    }
 }
 
 document.getElementById('description')
-  .addEventListener('keypress', (e) => {
-    const key = e.which || e.keyCode;
-    if (key === 13) {
-      e.preventDefault();
-      console.log('Digiou enter');
-    }
-  });
+    .addEventListener('keypress', (e) => {
+        const key = e.which || e.keyCode;
+        if (key === 13) {
+            e.preventDefault();
+        }
+    });
--- assets/getPlaylist.js
+++ assets/getPlaylist.js
@@ -1,23 +1,22 @@
 const request = require('request');
 require('dotenv').config();
 
-const urlPlaylist = "https://api.spotify.com/v1/me/playlists";
-const token = 'Bearer ' + process.env.TOKEN;
-request({url: urlPlaylist, method: 'GET', headers: {'Authorization': token, 'Content-Type': 'application/json'}},
-function (error, response, body) {
-    if(error) return;
-    const result = JSON.parse(body);
-    for(i=0; i < result.items.length; i++) {
-        createPlaylistName(result.items[i].name)        
-    }
-});
+const urlPlaylist = 'https://api.spotify.com/v1/me/playlists';
+const token = `Bearer ${process.env.TOKEN}`;
+request({ url: urlPlaylist, method: 'GET', headers: { Authorization: token, 'Content-Type': 'application/json' } },
+    (error, response, body) => {
+        if (error) return;
+        const result = JSON.parse(body);
+        for (i = 0; i < result.items.length; i++) {
+            createPlaylistName(result.items[i].name);
+        }
+    });
 
-function createPlaylistName(name){
+function createPlaylistName(name) {
     const playlist = document.querySelector('#playlist');
     const a = document.createElement('a');
     a.innerHTML = name;
     a.className = 'playlist-name btn';
-    a.href = '#' //Logo sera substituido pelo link da pagina
-    playlist.appendChild(a)    
+    a.href = '#'; // Logo sera substituido pelo link da pagina
+    playlist.appendChild(a);
 }
-
