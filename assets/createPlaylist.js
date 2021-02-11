e00ea40c98d4dcfe3f3ef9a018ad80725bc8da16
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-11 10:18:19 -0300

    Bloqueando a tecla enter do campo description

--- assets/createPlaylist.js
+++ assets/createPlaylist.js
@@ -11,7 +11,15 @@ function createPlaylist() {
     console.log(name)
     if(!name) {
         document.querySelector('.errorText').style.display = "block";
-        return
+        return;
     }
-
 }
+
+document.getElementById('description')
+  .addEventListener('keypress', function(e) {
+    let key = e.which || e.keyCode;
+    if (key === 13) {
+        e.preventDefault()
+      console.log('Digiou enter')
+    }
+  });
