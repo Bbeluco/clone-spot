51db0fd252c6782ca24cb77f5cbc9f7ced9eb8a1
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-18 15:05:20 -0300

    useState to save the token

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
 import { getTokenFromUrl } from './spotify';
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
   //based on a given condition run this piece of code
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = '';
-    const token = hash.access_token
+    const _token = hash.access_token
+
+    if(_token) setToken(_token)
+
     console.log('Your token is >>> ', token)
   }, []);
 
