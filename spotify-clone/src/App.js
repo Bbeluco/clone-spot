7c6a372575640d78e9cebf39aa3e61cd53401efe
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-18 14:54:08 -0300

    useeffect to load every time the pages load

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,14 +1,22 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  //based on a given condition run this piece of code
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    window.location.hash = '';
+    console.log('Your token is >>> ', token.access_token)
+  }, []);
+
   return (
     <div className="app">
       {/*Sporify Logo*/}
       {/*Login with Spotify */}
       <Login />
     </div>
-
   );
 }
 
