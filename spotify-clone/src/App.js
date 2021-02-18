d1d56bdbda628652c31f6bd6d7f8af8bd222f83f
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-18 11:59:28 -0300

    login page without react stuff

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -4,8 +4,6 @@ import Login from './Login';
 function App() {
   return (
     <div className="app">
-      <h1>Hi, i'm starting to editing app.js :rocket:</h1>
-
       {/*Sporify Logo*/}
       {/*Login with Spotify */}
       <Login />
--- spotify-clone/src/Login.js
+++ spotify-clone/src/Login.js
@@ -4,11 +4,10 @@ import './Login.css';
 function Login() {
     return (
         <div className='login'>
-            <h1>i'm the login page</h1>
             {/*Spotify Logo*/}
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="Spotify logo with spotify name"/>
             {/*Login with Spotify button */}
-            <button>LOGIN WITH SPOTIFY</button>
+            <a>LOGIN WITH SPOTIFY</a>
         </div>
     )
 }
