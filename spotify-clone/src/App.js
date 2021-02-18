6caab8279ab8f140f4767d21689ae4f7aa748c47
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-18 11:40:06 -0300

    starting with Login import/export

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
       <h1>Hi, i'm starting to editing app.js :rocket:</h1>
+
+      {/*Sporify Logo*/}
+      {/*Login with Spotify */}
+      <Login />
     </div>
+
   );
 }
 
---
+++ spotify-clone/src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return (
+        <div className='login'>
+            <h1>i'm the login page</h1>
+            {/*Spotify Logo*/}
+            {/*Login with Spotify button */}
+        </div>
+    )
+}
+
+export default Login
