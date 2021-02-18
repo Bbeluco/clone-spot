7170533dd88ac3f9872600b93c428506ed9c77a9
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-02-18 15:37:12 -0300

    Player component created

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
+import Player from './Player';
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
 
@@ -13,7 +17,15 @@ function App() {
     window.location.hash = '';
     const _token = hash.access_token
 
-    if(_token) setToken(_token)
+    if(_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log('person', user)
+      })
+    }
 
     console.log('Your token is >>> ', token)
   }, []);
@@ -22,7 +34,7 @@ function App() {
     <div className="app">
       {
         token ? (
-          <h1>I'm logged in</h1>
+          <Player />
         ) : (
           <Login />
         )
