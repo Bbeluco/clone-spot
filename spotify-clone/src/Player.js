5e234097b09b8b49e000c6537bff99adec254e80
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-02 16:59:35 -0300

    testing Body/Sidebar component

--- spotify-clone/src/Player.js
+++ spotify-clone/src/Player.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Player.css';
+import Sidebar from './Sidebar';
+import Body from './Body'
 
 function Player({ spotify }) {
     return (
-        <div>
-            <h1>i'm logged in</h1>
+        <div className="player">
+            <div className="player_body">
+                <Sidebar />
+                <Body />
+            </div>
+            {/* Footer */}
         </div>
     )
 }
