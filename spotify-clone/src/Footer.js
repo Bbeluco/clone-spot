376f21b9bd3eb014713877b9c6791cd32526fb9a
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-03 11:46:24 -0300

    aligning content

--- spotify-clone/src/Footer.js
+++ spotify-clone/src/Footer.js
@@ -4,7 +4,17 @@ import './Footer.css';
 function Footer() {
     return(
         <div class="footer">
-            <h1>I'm the footer</h1>
+            <div className="footer_left">
+                <p>i'm the left</p>
+            </div>
+
+            <div className="footer_center">
+                <p>I'm the center</p>
+            </div>
+
+            <div className="footer_right">
+                <p>i'm the right</p>
+            </div>
         </div>
     )
 }
