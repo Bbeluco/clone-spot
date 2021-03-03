fc3d0073c29d8a956eb4922692280cbace81b833
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-03 10:00:41 -0300

    Sidebar title without icons

--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
 
 function Sidebar(){
     return (
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo"/>
+            <SidebarOption title="Home" />
+            <SidebarOption title="Playlist" />
+            <SidebarOption title="Your Library" />
         </div>
     )
 }
