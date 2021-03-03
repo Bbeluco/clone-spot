703022b9ca86fa40d263247f3a47c54fdcb5604f
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-03 10:53:02 -0300

    sidebar title

--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -10,8 +10,12 @@ function Sidebar(){
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo"/>
             <SidebarOption title="Home" Icon={HomeIcon}/>
-            <SidebarOption title="Playlist" Icon={SearchIcon}/>
+            <SidebarOption title="Search" Icon={SearchIcon}/>
             <SidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
+
+            <br/>
+            <strong className="sidebar_title">PLAYLIST</strong>
+            <hr/>
         </div>
     )
 }
