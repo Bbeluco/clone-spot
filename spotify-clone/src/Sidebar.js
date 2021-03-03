ff0629cf31c43458ec462e5076bd34eee46d4a29
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-03 10:36:51 -0300

    sidebar title with icons

--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
 
 function Sidebar(){
     return (
         <div className="sidebar">
             <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo"/>
-            <SidebarOption title="Home" />
-            <SidebarOption title="Playlist" />
-            <SidebarOption title="Your Library" />
+            <SidebarOption title="Home" Icon={HomeIcon}/>
+            <SidebarOption title="Playlist" Icon={SearchIcon}/>
+            <SidebarOption title="Your Library" Icon={LibraryMusicIcon}/>
         </div>
     )
 }
