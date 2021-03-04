1b3c95ef231bb1b5ac40656118b1eac3e686d1e6
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-04 13:17:05 -0300

    body info format

--- spotify-clone/src/Body.js
+++ spotify-clone/src/Body.js
@@ -6,6 +6,16 @@ function Body({ spotify }){
     return(
         <div className="body">
             <Header spotify={spotify}/>
+
+            <div className="body_info">
+                <img src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png" alt=""/>
+
+                <div className="body_infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover weekly</h2>
+                    <p>description....</p>
+                </div>
+            </div>
         </div>
     )
 }
