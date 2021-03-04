7d5eb7e3899a2674336355d9db7f9e3d39eb31ec
Bbeluco <65624328+Bbeluco@users.noreply.github.com>
2021-03-04 13:12:38 -0300

    header done

--- spotify-clone/src/Header.js
+++ spotify-clone/src/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from './DataLayer';
 
 function Header({ spotify }) {
+    const [{ user }, dispatch] = useDataLayerValue();
+
     return(
         <div className="header">
             <div className="header_left">
@@ -11,8 +14,8 @@ function Header({ spotify }) {
                 <input placeholder="Search for Artists, Songs or Podcasts" type="text"/>
             </div>
             <div className="header_right">
-                <Avatar src="" alt="" />
-                <h4>beluc0</h4>
+                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
     );
